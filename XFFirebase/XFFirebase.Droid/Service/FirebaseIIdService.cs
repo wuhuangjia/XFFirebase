16ecf0b5a161587e5dee5d50fc5af01e20f019d9
wuhuangjia <wuhuangjia@gmail.com>
2018-06-22 18:10:13 +0800

    加入 FCM 支援

---
+++ XFFirebase/XFFirebase.Droid/Service/FirebaseIIdService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+
+namespace XFFirebase.Droid.Service
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    class FirebaseIIdService: FirebaseInstanceIdService
+    {
+        const string TAG = "taolecardFirebaseIIDService";
+
+        /**
+         * Called if InstanceID token is updated. This may occur if the security of
+         * the previous token had been compromised. Note that this is called when the InstanceID token
+         * is initially generated so this is where you would retrieve the token.
+         */
+        // [START refresh_token]
+        public override void OnTokenRefresh()
+        {
+            // Get updated InstanceID token.
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Android.Util.Log.Debug(TAG, $"Refreshed token:{refreshedToken}");
+
+            // TODO: Implement this method to send any registration to your app's servers.
+            SendRegistrationToServerAsync(refreshedToken);
+        }
+        // [END refresh_token]
+
+        /**
+         * Persist token to third-party servers.
+         *
+         * Modify this method to associate the user's FCM InstanceID token with any server-side account
+         * maintained by your application.
+         */
+        async void SendRegistrationToServerAsync(string token)
+        {
+            // Add custom implementation, as needed.
+            try
+            {
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
---
+++ XFFirebase/XFFirebase.Droid/Service/FirebaseMsgService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace XFFirebase.Droid.Service
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    class FirebaseMsgService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+            SendNotification(message.GetNotification().Title, message.GetNotification().Body, message.Data);
+        }
+        void SendNotification(string messageTitle, string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (string key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Drawable.icon)
+                .SetContentTitle(messageTitle)
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+
+    }
+}
